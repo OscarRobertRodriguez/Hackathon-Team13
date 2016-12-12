2049c8b4d2523b112d1e308b62b09dca64de82ed
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2016-12-11 18:06:40 -0600

    made changes

---
+++ inputs/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by oscar on 12/11/16.
+ */
+
+
+var inputListener = document.getElementsByClassName("inputs");
+
+for(var i = 0; i < inputListener.length; i++) {
+   inputListener[i].addEventListener("keydown", isNotEmpty);
+}
+
+
+function isNotEmpty(ele) {
+   var input = ele.target;
+   var inputText = input.value;
+
+   if (inputText.length)
+
+}
